686376910c0a3ee3f319f61a424a424c51c8cd88
Quentin Harley <quentin.harley@gmail.com>
2016-12-19 09:29:23 +0200

    Return ZGrid Initial commit

--- src/modules/tools/zprobe/ZProbe.cpp
+++ src/modules/tools/zprobe/ZProbe.cpp
@@ -30,6 +30,7 @@
 // strategies we know about
 #include "DeltaCalibrationStrategy.h"
 #include "ThreePointStrategy.h"
+#include "ZGridStrategy.h"
 #include "DeltaGridStrategy.h"
 
 #define enable_checksum          CHECKSUM("enable")
@@ -98,6 +99,11 @@ void ZProbe::config_load()
                     this->strategies.push_back(new ThreePointStrategy(this));
                     found= true;
                     break;
+
+                case ZGrid_leveling_checksum:
+                     this->strategies.push_back(new ZGridStrategy(this));
+                     found= true;
+                     break;
 
                 case delta_grid_leveling_strategy_checksum:
                     this->strategies.push_back(new DeltaGridStrategy(this));
